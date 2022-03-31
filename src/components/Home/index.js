83a83bd41b9004e059d67ad13368979e1611abe1
wanglei <806416060@qq.com>
2022-03-31 08:34:41 +0800

    增加路由的基本使用

---
+++ src/components/Home/index.js
@@ -0,0 +1,17 @@
+/*
+ * @Author       : wanglei
+ * @Date         : 2022-03-30 21:01:35
+ * @LastEditors  : wanglei
+ * @LastEditTime : 2022-03-30 21:06:41
+ * @FilePath     : /shangguigu-todolist/src/components/Home/index.js
+ * @description  : 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>我是Home的内容</div>
+    )
+  }
+}
